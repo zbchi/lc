b3e389b1c15c96ec7bf0a128d78a90037d756efe
zb <1662308219@qq.com>
2025-01-21 16:20:58 +0800

    daily

--- 0121找出字符串中第一个匹配项的下标.c
+++ 0121找出字符串中第一个匹配项的下标.c
@@ -1,16 +1,7 @@
-#include<stdio.h>
-#include<math.h>
-#include<stdbool.h>
 int strStr(char* haystack, char* needle) {
     char* result =NULL;
     result = strstr(haystack,needle);
     if(result==NULL)
     return -1;
     return result - haystack+1;
-}
-
-int main()
-{
-   printf("%d",strStr("sadbutsad","sad"));
-   return 0;
 }
